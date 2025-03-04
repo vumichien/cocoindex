20564d4f8a84368816087088ad9c4494f1b1709c
LJ <linghua@cocoindex.io>
2025-03-03 18:53:43 -0800

    fix docs link to github (#3)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/cocoIndex/cocoindex/tree/main/docs',
+          editUrl: 'https://github.com/cocoindex-io/cocoindex/tree/main/docs',
         },
         blog: false,
         theme: {
@@ -115,7 +115,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/cocoindex',
+              href: 'https://github.com/cocoindex-io/cocoindex',
             },
             {
               label: 'Discord Community',
