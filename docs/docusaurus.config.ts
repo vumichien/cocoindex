2cb46a3f29dd84eb5484fc0cdc01e2880710de4c
LJ <linghua@cocoindex.io>
2025-03-08 11:36:01 -0800

    update prism-react-renderer version (#71)
    * update prism-react-renderer version
    
    * revert project anme to docs
    
    * use mermaind theme
    
    * revert mermaind

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -137,6 +137,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['diff', 'json', 'bash', 'docker'],
     },
   } satisfies Preset.ThemeConfig,
 };
