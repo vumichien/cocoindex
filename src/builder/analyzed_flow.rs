4a345cd57e5e24628402ccfef54c5e616b30e7ca
LJ <linghua@cocoindex.io>
2025-03-15 17:17:03 -0700

    Cleanup unnecessary traces. (#138)

--- src/builder/analyzed_flow.rs
+++ src/builder/analyzed_flow.rs
@@ -10,7 +10,6 @@ use crate::{
 };
 use anyhow::Result;
 use futures::{future::Shared, FutureExt};
-use log::trace;
 
 pub struct AnalyzedFlow {
     pub flow_instance: spec::FlowInstanceSpec,
@@ -56,7 +55,6 @@ impl AnalyzedFlow {
     }
 
     pub async fn get_execution_plan(&self) -> Result<Arc<plan::ExecutionPlan>> {
-        trace!("get_execution_plan() started");
         let execution_plan = self
             .execution_plan
             .as_ref()
@@ -64,7 +62,6 @@ impl AnalyzedFlow {
             .clone()
             .await
             .std_result()?;
-        trace!("get_execution_plan() finished");
         Ok(execution_plan)
     }
 }
